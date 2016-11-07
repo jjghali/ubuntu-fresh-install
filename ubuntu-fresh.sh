f76336e57529c37ba1aeed1d8a077ec428b9bbf4
Joshua Ghali <jjghali@gmail.com>
2016-11-07 12:58:11 -0500

    added packages

--- ubuntu-fresh.sh
+++ ubuntu-fresh.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+#URLS
+$androidstudio="https://dl.google.com/dl/android/studio/ide-zips/2.2.2.0/android-studio-ide-145.3360264-linux.zip"
+$clion="https://download.jetbrains.com/cpp/CLion-2016.2.2.tar.gz"
+$intellij="https://download.jetbrains.com/idea/ideaIU-2016.2.5.tar.gz"
+$privateinternetaccess="https://www.privateinternetaccess.com/installer/install_ubuntu.sh"
+$visualstudiocode="https://az764295.vo.msecnd.net/stable/02611b40b24c9df2726ad8b33f5ef5f67ac30b44/code_1.7.1-1478180561_amd64.deb"
+$atom="https://atom-installer.github.com/v1.10.2/atom-amd64.deb"
+$gitkraken="https://release.gitkraken.com/linux/gitkraken-amd64.deb"
+$insync="http://s.insynchq.com/builds/insync_1.3.11.36106-trusty_amd64.deb"
+$teamspeak="http://dl.4players.de/ts/releases/3.0.19.4/TeamSpeak3-Client-linux_amd64-3.0.19.4.run"
+
 echo "-------------------------------"
 echo "Welcome to your fresh "
 
@@ -37,9 +48,9 @@ sudo apt install chromium-browser -y
 sleep 1
 
 echo "-------------------------------"
-echo "Installing LibreOffice"
+echo "Installing gdebi"
 echo "-------------------------------"
-sudo apt install libreoffice gdebi -y
+sudo apt install gdebi -y
 sleep 1
 
 echo "-------------------------------"
@@ -52,7 +63,7 @@ echo "-------------------------------"
 echo "Installing privateinternetaccess VPN"
 echo "-------------------------------"
 
-wget https://www.privateinternetaccess.com/installer/install_ubuntu.sh
+wget $privateinternetaccess
 sudo sh ./install_ubuntu.sh
 rm install_ubuntu.sh
 
@@ -65,9 +76,9 @@ cd ~/.tools
 
 mkdir ~/.tools/android-studio
 #Download apps
-wget https://download.jetbrains.com/idea/ideaIU-2016.2.4.tar.gz -O idea.tar.gz
-wget https://download.jetbrains.com/cpp/CLion-2016.2.2.tar.gz -O clion.tar.gz
-wget https://dl.google.com/dl/android/studio/ide-zips/2.2.1.0/android-studio-ide-145.3330264-linux.zip -O android-studio.zip
+wget $intellij -O idea.tar.gz
+wget $clion -O clion.tar.gz
+wget $androidstudio -O android-studio.zip
 
 echo "-------------------------------"
 echo "Installing Jetbrains tools"
@@ -93,7 +104,7 @@ echo "Installing dev tools"
 echo "-------------------------------"
 
 cd ~/Downloads
-wget https://az764295.vo.msecnd.net/stable/66f37fd2a99eb9d628dd374d81d78835b410c39b/code_1.5.2-1473686317_amd64.deb -O visualstudiocode.deb
+wget $visualstudiocode -O visualstudiocode.deb
 sleep 1
 sudo gdebi visualstudiocode.deb
 sudo apt install openjdk-8-jre openjdk-8-jdk -y
@@ -103,18 +114,31 @@ echo "-------------------------------"
 echo "Installing atom"
 echo "-------------------------------"
 
-wget https://atom-installer.github.com/v1.10.2/atom-amd64.deb -O atom.deb
+wget $atom -O atom.deb
 sudo gdebi atom.deb
 sleep 1
 
-wget https://release.gitkraken.com/linux/gitkraken-amd64.deb -O gitkraken.deb
+
+echo "-------------------------------"
+echo "Installing atom"
+echo "-------------------------------"
+
+
+cd ~/.tools
+wget $teamspeak -O temaspeak.run
+chmod +x teamspeak.run
+./teamspeak.run
+
+sleep 1
+
+wget $gitkraken -O gitkraken.deb
 sudo gdebi gitkraken.deb
 
 echo "-------------------------------"
 echo "Installing Insync"
 echo "-------------------------------"
 
-wget http://s.insynchq.com/builds/insync_1.3.11.36106-trusty_amd64.deb -O insync.deb
+wget $insync -O insync.deb
 sudo gdebi insync.deb
 sleep 1
 
@@ -131,7 +155,24 @@ echo "-------------------------------"
 echo "Installing other stuff"
 echo "-------------------------------"
 
-sudo apt install redshift gtk-redshift htop powertop zsh curl guake virtualbox -y
+sudo apt install redshift gtk-redshift htop powertop zsh curl guake virtualbox gstreamer-1.0-plugins-ugly -y
+
+echo "-------------------------------"
+echo "Installing papirus icon theme and Adapta"
+echo "-------------------------------"
+
+sudo apt-add-repository ppa:tista/adapta
+sudo add-apt-repository ppa:varlesh-l/papirus-pack
+sudo apt update
+sudo apt install adapta-gtk-theme papirus-icons unity-tweak-tool -y
+
+mkdir ~/git-repos
+
+echo "-------------------------------"
+echo "Installing oh my zsh"
+echo "-------------------------------"
+
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 echo "-------------------------------"
 echo "Cleaning up"
